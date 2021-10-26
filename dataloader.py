0aa9857b81ca9625e019191d12a357f2582d600e
David Franz <davidfranznz@gmail.com>
2021-10-27 04:03:21 +1300

    tidied code, musk working well, vehicle really needs bug where falls to 0 features selected fixed

--- dataloader.py
+++ dataloader.py
@@ -1,31 +1,50 @@
+import sys
+
 import pandas as pd
 
 class DataLoader:
 
 	@staticmethod
 	def load_data(filename):
-		
-		# this will automatically close the file when we exit the block
-		with open("data/musk/" + str(filename), "r") as f:
-			lines = [line.split() for line in f.readlines()]
-			str_list = [list(line[0].split(',')) for line in lines]
-			
-			data_list = list()
-			for l in str_list:
-				data_entry = list()
-				data_class = "non-musk" if "non-musk" in l[0].lower() else "musk"
-				conformation_name = l[1]
-				for i, feature in enumerate(l):
-					if i == 0 or i == 1 or i == (len(l)-1):
-						continue
-					data_entry.append(int(feature))
-			
-				#data_entry.append(conformation_name)
-				data_entry.append(data_class)
-				data_list.append(data_entry)
-			
-			data_dict = dict()
-			for index, line in enumerate(data_list):
-				data_dict[index] = line
 
-			return pd.DataFrame(data_dict)
+		if "musk" in filename:
+			# this will automatically close the file when we exit the block
+			with open("data/" + str(filename), "r") as f:
+				lines = [line.split() for line in f.readlines()]
+				str_list = [list(line[0].split(',')) for line in lines]
+				
+				data_list = list()
+				for l in str_list:
+					data_entry = list()
+					data_class = "non-musk" if "non-musk" in l[0].lower() else "musk"
+					conformation_name = l[1]
+					for i, feature in enumerate(l):
+						if i == 0 or i == 1 or i == (len(l)-1): # have a think about this line
+							continue
+						data_entry.append(int(feature))
+				
+					#data_entry.append(conformation_name)
+					data_entry.append(data_class)
+					data_list.append(data_entry)
+				
+				data_dict = dict()
+				for index, line in enumerate(data_list):
+					data_dict[index] = line
+
+				return pd.DataFrame(data_dict)
+		
+		elif "vehicle" in filename:
+			# this will automatically close the file when we exit the block
+			with open("data/" + str(filename), "r") as f:
+				lines = [line.split() for line in f.readlines()]
+				
+				data_list = [[str(line[i]) if i == (len(line)-1) else int(line[i]) for i in range(len(line))] for line in lines]
+				
+				data_dict = dict()
+				for index, line in enumerate(data_list):
+					data_dict[index] = line
+
+				return pd.DataFrame(data_dict)
+
+		else:
+			pass # throw exception here
--- nsga2_FS.py
+++ nsga2_FS.py
@@ -3,33 +3,31 @@ import random
 
 import numpy as np
 import pandas as pd
+import matplotlib as plt
 
+from functools import cmp_to_key
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from pymoo.optimize import minimize
 from pymoo.core.problem import Problem
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.factory import get_crossover, get_mutation, get_sampling
+from pymoo.indicators.hv import Hypervolume
 
 from dataloader import DataLoader
 
-NUMBER_OF_FEATURES = 167 # this is for the musk data set
-NUMBER_OF_CLASSES = 2 # this is for the musk data set
-
-# design is a binary string
 def fitness(binary_string):
 	return wrapper_based_fitness_function(binary_string), (count_1s(binary_string)/NUMBER_OF_FEATURES)
 
 class ProblemWrapper(Problem):
 
 	def _evaluate(self, designs, out, *args, **kwargs):
-		#for i in designs:
-			#print(i.astype(int))
-
 		res = list()
 		for design in designs:
-			res.append(fitness(''.join([str(i) for i in design.astype(int)]))) # need to define this fitness function
-		
+			
+			bs = ''.join([str(i) for i in design.astype(int)])
+			res.append(fitness(bs))
+
 		out['F'] = np.array(res) # F is for function output
 
 def convert_df_to_list(data):
@@ -44,13 +42,12 @@ def convert_df_to_list(data):
 
 def wrapper_based_fitness_function(binary_string):
 	if binary_string == ('0' * len(binary_string)):
-		return 0
+		return float('-inf')
 
 	filter_data = lambda data_as_list: [[data_as_list[j][i] for i in range(len(data_as_list[0])-1)] for j in range(len(data_as_list))]
-	get_class_labels = lambda data_as_list: [1 if data_as_list[i][len(data_as_list[0])-1]=="musk" else 0 for i in range(len(data_as_list))]
+	get_class_labels = lambda data_as_list: [class_numerical_mapping[data_as_list[i][len(data_as_list[0])-1]] for i in range(len(data_as_list))]
 
 	training_df_as_list = convert_df_to_list(training_data.T)
-
 	training_data_filtered = filter_data(training_df_as_list)
 	training_class_labels = get_class_labels(training_df_as_list)
 
@@ -58,7 +55,6 @@ def wrapper_based_fitness_function(binary_string):
 	knn.fit(training_data_filtered, training_class_labels)
 
 	testing_df_as_list = convert_df_to_list(testing_data.T)
-
 	testing_data_filtered = filter_data(testing_df_as_list)
 	testing_class_labels = get_class_labels(testing_df_as_list)
 
@@ -69,27 +65,35 @@ def wrapper_based_fitness_function(binary_string):
 		if predicted_class_labels[i] == testing_class_labels[i]:
 			count_correct += 1
 
-	return float(count_correct) / len(predicted_class_labels)
+	#print(float(count_correct))
+	#print(len(predicted_class_labels))
+
+	fitness = float(count_correct) / len(predicted_class_labels)
+
+	return fitness
 
 def count_1s(binary_string):
 	return sum([int(i) for i in list(binary_string)])
 
 if __name__ == '__main__':
 
-	df = DataLoader.load_data("clean1.data")
+	f = "musk/clean1.data"
+	#f = "vehicle/vehicle.dat"
+
+	NUMBER_OF_FEATURES = 167 if "musk" in f else 18 # this is for the musk data set
+	class_numerical_mapping = {"non-musk":0, "musk":1} if "musk" in f else{"van":0, "saab":1, "bus":2, "opel":3}
+
+	df = DataLoader.load_data(f)
+
+	print(df)
 	
-	seed = random.randint(0,100)
+	training_data, testing_data = train_test_split(df.T, test_size=0.2, random_state=random.randint(0,100))
 
-	training_data, testing_data = train_test_split(df.T, test_size=0.2, random_state=seed)
-	
+	problem = ProblemWrapper(n_var=NUMBER_OF_FEATURES, n_obj=2, xl=0, xu=1, type_var=bool) # xl is lower bound, xu is upper bound
 
-	bs = ''.join([str(random.randint(0,1)) for i in range(NUMBER_OF_FEATURES)])
+	algorithm = NSGA2(pop_size=5, sampling=get_sampling("bin_random"), crossover=get_crossover("bin_hux"), mutation=get_mutation("bin_bitflip"), eliminate_duplicates=True)
 
-	problem = ProblemWrapper(n_var=167, n_obj=2, xl=0, xu=1, type_var=bool) # xl is lower bound, xu is upper bound
-
-	algorithm = NSGA2(pop_size=50, sampling=get_sampling("bin_random"), crossover=get_crossover("bin_hux"), mutation=get_mutation("bin_bitflip"), eliminate_duplicates=True)
-
-	stop_criteria = ('n_gen', 25) # number_of_generations=100 is our stopping criteria
+	stop_criteria = ('n_gen', 5)
 
 	results = minimize(problem=problem, algorithm=algorithm, termination=stop_criteria)
 
@@ -103,5 +107,3 @@ if __name__ == '__main__':
 		fitnesses.append(wrapper_based_fitness_function(result))
 
 	print(fitnesses)
-
-
